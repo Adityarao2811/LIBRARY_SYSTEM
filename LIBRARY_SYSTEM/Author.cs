f278ff26d0b2756f181aa19e2b137d04937a07a2
Aditya Rao <a_rao107197@fanshaweonline.ca>
2021-11-26 21:50:23 +0530

    Add project files.

---
+++ LIBRARY_SYSTEM/Author.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LIBRARY_SYSTEM
+{
+    public class Author
+    {
+        string Name;
+        List<string> Books;
+        Dictionary<string, string> Borrowers;
+        static int NumberOfAuthors = 0;
+        public Author(string n)
+        {
+            this.Name = n;
+            this.Books = new List<string>();
+            this.Borrowers = new Dictionary<string, string>();
+            NumberOfAuthors++;
+        }
+        public void DisplayBorrowersList()
+        {
+            if(this.Borrowers.Count==0)
+            {
+                Console.WriteLine(this.Name + " books are not borrowed");
+                return;
+            }
+            foreach(var f in this.Borrowers.Keys)
+            {
+                Console.WriteLine(this.Borrowers[f]);
+            }
+        }
+        public void DisplayAvailableBooks()
+        {
+            foreach(string b in this.Books)
+            {
+                if(!this.Borrowers.ContainsKey(b))
+                {
+                    Console.WriteLine(b);
+                }
+            }
+        }
+        public void AddBook(string b)
+        {
+            this.Books.Add(b);
+            Console.WriteLine("Book " + b + " has been added to " + this.Name);
+        }
+        public void AddBorrowerandBook(string book,string borrower)
+        {
+            if (this.Books.Contains(book))
+            {
+                if (this.Borrowers.ContainsKey(book))
+                {
+                    Console.WriteLine("This book is already borrowed");
+                }
+                else
+                {
+                    this.Borrowers[book] = borrower;
+                }
+            }
+            else
+            {
+                Console.WriteLine("This book is not present");
+            }
+        }
+        public void ReturnBook(string book)
+        {
+            if (!this.Borrowers.ContainsKey(book))
+            {
+                Console.WriteLine("This book is not borrowed");
+            }
+            else
+            {
+                this.Borrowers.Remove(book);
+            }
+        }
+        public void DisplayBooks()
+        {
+            if (this.Books.Count == 0)
+            {
+                Console.WriteLine(this.Name+" has No Books");
+            }
+            else
+            {
+                Console.WriteLine("Below are the books of " + this.Name);
+                foreach (string b in this.Books)
+                {
+                    Console.WriteLine(b);
+                }
+            }
+        }
+        public static void WhoAreAuthors()
+        {
+            Console.WriteLine("Authros write Books");
+        }
+        public static void DisplayNumberOfAuthors()
+        {
+            Console.WriteLine(Author.NumberOfAuthors);
+        }
+        public static Author FindAuthor(List<Author> authors,string name)
+        {
+            return authors.Find(a => a.Name == name);
+        }
+        }
+    }
---
+++ LIBRARY_SYSTEM/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LIBRARY_SYSTEM
+{
+    class Program
+    {
+        static string name;
+        static Author a;
+        static List<Author> authorsList = new List<Author>();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(" Enter 1 to add a book\n Enter 2 to view books\n Enter 3 to borrow a book\n Enter 4 to return a book\n Enter 5 to add an Author\n Enter 6 to display authors\n Enter 7 to see available books\n Enter 8 to display Borrowers List\n Enter 9 to exit");
+                string option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        AddBook();
+                        break;
+                    case "2":
+                        DisplayBooks();
+                        break;
+                    case "3":
+                        AddBorrowerandBook();
+                        break;
+                    case "4":
+                        ReturnBook();
+                        break;
+                    case "5":
+                        AddAuthor();
+                        break;
+                    case "6":
+                        if (authorsList.Count == 0)
+                        {
+                            Console.WriteLine("No Authors");
+                            break;
+                        }
+                        foreach (Author s in authorsList)
+                        {
+                            s.DisplayBooks();
+                            Console.WriteLine("**********");
+                        }
+                        break;
+                    case "7":
+                        DisplayAvailableBooks();
+                        break;
+                    case "8":
+                        DisplayBorrowersList();
+                        break;
+                    case "9":
+                        Console.WriteLine("Goodbye. Have a nice day!\n ______________________________________________________");
+                        System.Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Input");
+                        break;
+                }
+                Console.WriteLine("________________________________________________________________________________");
+            }
+        }
+
+        private static void DisplayBorrowersList()
+        {
+            foreach (Author s in authorsList)
+            {
+                s.DisplayBorrowersList();
+            }
+
+        }
+
+        private static void DisplayAvailableBooks()
+        {
+            foreach (Author s in authorsList)
+            {
+                s.DisplayAvailableBooks();
+            }
+        }
+
+        private static void AddAuthor()
+        {
+            Console.WriteLine("Enter the Author's name");
+            name = Console.ReadLine();
+            a = Author.FindAuthor(authorsList, name);
+            if (a != null)
+            {
+                Console.WriteLine("This Author already exists");
+                return;
+            }
+            Author newAuthor = new Author(name);
+            authorsList.Add(newAuthor);
+        }
+
+        private static void ReturnBook()
+        {
+            Console.WriteLine("Enter the Author's name");
+            name = Console.ReadLine();
+            a = Author.FindAuthor(authorsList, name);
+            if (a == null)
+            {
+                Console.WriteLine("This Author does not exist");
+                return;
+            }
+            Console.WriteLine("Enter the name of book");
+            a.ReturnBook(Console.ReadLine());
+        }
+
+        private static void AddBorrowerandBook()
+        {
+            Console.WriteLine("Enter the Author's name");
+            name = Console.ReadLine();
+            a = Author.FindAuthor(authorsList, name);
+            if (a == null)
+            {
+                Console.WriteLine("This Author does not exist");
+                return;
+            }
+            Console.WriteLine("Enter the name of book");
+            string book = Console.ReadLine();
+            Console.WriteLine("Enter your name");
+            string borrower = Console.ReadLine();
+            a.AddBorrowerandBook(book, borrower);
+        }
+
+        private static void DisplayBooks()
+        {
+            Console.WriteLine("Enter the Author's name");
+            name = Console.ReadLine();
+            a = Author.FindAuthor(authorsList, name);
+            if (a == null)
+            {
+                Console.WriteLine("This Author does not exist");
+                return;
+            }
+            a.DisplayBooks();
+        }
+
+        private static void AddBook()
+        {
+            Console.WriteLine("Enter the Author's name");
+            name = Console.ReadLine();
+            a = Author.FindAuthor(authorsList, name);
+            if (a == null)
+            {
+                Console.WriteLine("This Author does not exist");
+                return;
+            }
+            Console.WriteLine("Enter the book name");
+            a.AddBook(Console.ReadLine());
+        }
+    }
+}
